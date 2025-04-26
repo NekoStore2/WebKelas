38488f770a5b64d3ba722a9d3ce0279198a7cbac
NekoStore2 <164801435+NekoStore2@users.noreply.github.com>
2025-04-26 18:43:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleTheme() {
+  document.documentElement.classList.toggle('dark');
+  document.documentElement.classList.toggle('bg-white');
+  document.documentElement.classList.toggle('bg-gray-900');
+  document.documentElement.classList.toggle('text-black');
+  document.documentElement.classList.toggle('text-white');
+}
